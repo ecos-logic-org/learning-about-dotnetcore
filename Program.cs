647ea68ff2ab0e889c5268369691814d87dc4767
Pablo C. Vergara Castro <pablo.vergara@digitalizat.com>
2020-10-03 10:31:30 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppPRJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
